c2cb10f7074707cdf9a1dbe0d86a5d389e05480b
Lasisi Kehinde <56618398+kenny15335@users.noreply.github.com>
2020-11-21 09:00:15 +0100

    Mail fixed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import Header from "../../components/Header/index";
+import emailjs from "emailjs-com";
 import "./Home.css";
 import Slider from "react-slick";
 import CarouselItem from "../../components/CarouselItem/CarouselItem";
@@ -9,8 +10,13 @@ import img from "../../assets/images/carousel-img.png";
 import img2 from "../../assets/images/carousel-img2.png";
 import img3 from "../../assets/images/carousel-img3.png";
 import img1 from "../../assets/images/carousel-img1.png";
-import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
-import { motion } from "framer-motion";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaCheckCircle,
+} from "react-icons/fa";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -45,7 +51,7 @@ const settings = {
   slidesToScroll: 1,
   autoplay: true,
   arrows: false,
-  autoplaySpeed: 2000
+  autoplaySpeed: 2000,
 };
 
 const carouselList = data.map((data, index) => (
@@ -58,6 +64,29 @@ const carouselList = data.map((data, index) => (
 ));
 
 const Home = () => {
+  const [email, setEmail] = useState("");
+  const [status, setStatus] = useState(undefined);
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_GMAIL_SERVICE_CODE,
+        process.env.REACT_APP_MAIL_TEMPLATE,
+        e.target,
+        process.env.REACT_APP_GMAIL_SERVICE_ID
+      )
+      .then(
+        (result) => {
+          setStatus(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    setEmail("");
+  }
+
   return (
     <>
       <Container fluid>
@@ -67,12 +96,12 @@ const Home = () => {
             <h1 className="text-center banner_note text-white-50">
               Site under Construction
             </h1>
-            <motion.h2
+            <h2
               className="text-white text-center leading-4 banner_main pt-3"
-              animate={{ color: '#400',opacity: 0.8}}
+              animate={{ color: "#400", opacity: 0.8 }}
             >
               COMING SOON
-            </motion.h2>
+            </h2>
             <Col as="div" className="carousel_main_container ">
               {/* CAROUSEL */}
 
@@ -85,13 +114,32 @@ const Home = () => {
                 Join us in making music fun again
               </h2>
               <Col as="div" className="mx-auto input-cont">
-                <input
-                  type="text"
-                  className="banner_input"
-                  placeholder="Enter Your Email"
-                  className="banner_input"
-                />
-                <button className="banner_email_btn">SEND</button>
+                <form
+                  onSubmit={sendEmail}
+                  className="d-flex w-full justify-between input-cont2 mx-auto"
+                >
+                  <input
+                    type="email"
+                    className="banner_input"
+                    placeholder="Enter Your Email"
+                    required
+                    className="banner_input"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <button
+                    className="banner_email_btn"
+                    type="submit"
+                    disabled={!email }
+                  >
+                    {!status ? (
+                      "SEND"
+                    ) : (
+                      <FaCheckCircle color="green" size={20} />
+                    )}
+                  </button>
+                </form>
               </Col>
               <Col as="div" className="banner_footer">
                 <h2 className="banner_footer_title text-center text-white-50  mb-2">
